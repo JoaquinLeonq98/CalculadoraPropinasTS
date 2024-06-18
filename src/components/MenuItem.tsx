c158e1cab1bc92e8b129e4283ad151628f1a181a
joaquin leon <joaquinleonquero00@gmail.com>
2024-06-18 12:27:56 -0600

    linux 2024-06-18

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,14 +1,16 @@
 import type { MenuItem } from "../types"
 
 type MenuItemProps = {
-    item: MenuItem
+  item: MenuItem
+  addItem: (item: MenuItem) => void
 }
 
-export default function MenuItem({item} : MenuItemProps) {
+export default function MenuItem({ item, addItem }: MenuItemProps) {
   return (
-    <button className="border border-teal-400 w-full hover:bg-teal-300 p-3 flex justify-between">
-        <p>{item.name}</p>
-        <p className="font-black">${item.price}</p>
+    <button className="border border-teal-400 w-full hover:bg-teal-300 p-3 flex justify-between"
+      onClick={() => addItem(item)}>
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
     </button>
   )
 }
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,15 +1,15 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
+
 
 export default function useOrder() {
-    const [order, setOrder] = useState<OrderItem[]>([])
-    
-    const addItem = () => {
-        console.log('add item')
-    }
+  const [order, setOrder] = useState<OrderItem[]>([])
+  const addItem = (item: MenuItem) => {
+    console.log(item)
+  }
 
 
-    return {
-        addItem
-    }
+  return {
+    addItem
+  }
 }
